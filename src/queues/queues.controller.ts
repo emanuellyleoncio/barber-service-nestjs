b6aa63ff0b833f0c81dd9767c787b3933b467fe4
emanuellyleoncio <manuleoncio01@gmail.com>
2024-02-20 07:55:28 -0400

    create queues controller, service structure and create queue endpoint

---
+++ src/queues/queues.controller.ts
@@ -0,0 +1,42 @@
+import {
+	BadRequestException,
+	Body,
+	Controller,
+	HttpStatus,
+	NotFoundException,
+	Post,
+	Res
+} from '@nestjs/common'
+import { QueuesService } from './queues.service'
+import CreateQueueDto from './dtos/create-queue'
+import { Response } from 'express'
+import { ExpertsService } from 'src/experts/experts.service'
+
+@Controller('queues')
+export class QueuesController {
+	constructor(
+		private readonly queuesService: QueuesService,
+		private readonly expertsService: ExpertsService
+	) {}
+
+	@Post()
+	async createQueue(@Body() data: CreateQueueDto, @Res() res: Response) {
+		const expert = await this.expertsService.findExpert(data.expertId)
+		if (!expert) {
+			throw new NotFoundException('O profissional não existe.')
+		}
+
+		const queueExists = await this.queuesService.queueExpertExistsToday(
+			data.expertId
+		)
+
+		if (queueExists) {
+			throw new BadRequestException(
+				'Já existe uma fila para o profissional na data atual.'
+			)
+		}
+
+		const queue = await this.queuesService.createQueue(data)
+		return res.status(HttpStatus.CREATED).json(queue)
+	}
+}
